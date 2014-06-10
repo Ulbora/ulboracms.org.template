85c226f06ce1991293ae7d443ef916ad9045a227
ken <ken@bigMo>
2014-06-09 20:58:11 -0400

    initial

---
+++ AngularBook/AngularJsBlogChapter9/public_html/js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/* App Module */
+
+var angularBlogApp = angular.module('angularBlogApp', [
+    'ngRoute',     
+    'angularBlogControllers',
+    'angularBlogServices',
+    'angularBlogBusinessServices',
+    'angularBlogDirectives'
+    
+]);
+
+
+angularBlogApp.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $routeProvider.
+                when('/', {
+                    templateUrl: 'partials/main.html',
+                    controller: 'BlogCtrl'
+                }).when('/blogPost/:id', {
+                    templateUrl: 'partials/blogPost.html',
+                    controller: 'BlogViewCtrl'
+                });
+
+        $locationProvider.html5Mode(false).hashPrefix('!');
+    }]);
+
+
+
---
+++ AngularBook/AngularJsBlogChapter9/public_html/js/controllers.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/* Controllers */
+
+var angularBlogControllers = angular.module('angularBlogControllers', []);
+
+
+
+angularBlogControllers.controller('BlogCtrl', ['$scope', 'BlogList',
+    function BlogCtrl($scope, BlogList) {
+        BlogList.get({},
+                function success(response) {
+                    //alert($scope.challenge.question);
+                    console.log("Success:" + JSON.stringify(response));
+                    $scope.blogList = response;
+
+                },
+                function error(errorResponse) {
+                    console.log("Error:" + JSON.stringify(errorResponse));
+                }
+        );
+
+
+    }]);
+
+angularBlogControllers.controller('BlogViewCtrl', ['$scope', '$routeParams', 'BlogPost',
+    function BlogViewCtrl($scope, $routeParams, BlogPost) {
+        var blogId = $routeParams.id;
+        
+        BlogPost.get({id: blogId},
+                function success(response) {
+                    //alert($scope.challenge.question);
+                    console.log("Success:" + JSON.stringify(response));
+                    $scope.blogEntry = response;
+
+                },
+                function error(errorResponse) {
+                    console.log("Error:" + JSON.stringify(errorResponse));
+                }
+        );
+
+    }]);
+
