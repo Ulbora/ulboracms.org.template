ef1709c300461fafaccd17b2e3270cc837a4b54b
ken <ken@bigMo>
2014-11-13 22:33:14 -0500

    updates for testing

--- AngularBook/AngularJsBlogChapter10/public_html/js/controllers.js
+++ AngularBook/AngularJsBlogChapter10/public_html/js/controllers.js
@@ -8,9 +8,10 @@ var angularBlogControllers = angular.module('angularBlogControllers', []);
 
 angularBlogControllers.controller('BlogCtrl', ['$scope', 'BlogList', '$location', 'checkCreds',
     function BlogCtrl($scope, BlogList, $location, checkCreds) {
-        if(!checkCreds()){
-            $location.path('/login'); 
+        if (!checkCreds()) {
+            $location.path('/login');
         }
+        $scope.blogList = [];
         BlogList.get({},
                 function success(response) {
                     //alert($scope.challenge.question);
@@ -28,18 +29,18 @@ angularBlogControllers.controller('BlogCtrl', ['$scope', 'BlogList', '$location'
 
 angularBlogControllers.controller('BlogViewCtrl', ['$scope', '$routeParams', 'BlogPost', '$location', 'checkCreds',
     function BlogViewCtrl($scope, $routeParams, BlogPost, $location, checkCreds) {
-        if(!checkCreds()){
-            $location.path('/login'); 
+        if (!checkCreds()) {
+            $location.path('/login');
         }
         var blogId = $routeParams.id;
-        
+        $scope.blg = 1;
         BlogPost.get({id: blogId},
-                function success(response) {
-                    //alert($scope.challenge.question);
-                    console.log("Success:" + JSON.stringify(response));
-                    $scope.blogEntry = response;
+        function success(response) {
+            //alert($scope.challenge.question);
+            console.log("Success:" + JSON.stringify(response));
+            $scope.blogEntry = response;
 
-                },
+        },
                 function error(errorResponse) {
                     console.log("Error:" + JSON.stringify(errorResponse));
                 }
@@ -47,35 +48,37 @@ angularBlogControllers.controller('BlogViewCtrl', ['$scope', '$routeParams', 'Bl
 
     }]);
 
-angularBlogControllers.controller('LoginCtrl', ['$scope', '$location','Login', 'setCreds',
-    function LoginCtrl($scope, $location, Login, setCreds) { 
-        $scope.submit = function(){
-             var postData = {
-                 "username" : $scope.username,
-                 "password" : $scope.password
-             };
-            Login.login({}, postData,
-                function success(response) {                    
-                    console.log("Success:" + JSON.stringify(response));                   
-                    if(response.authenticated){
-                        setCreds($scope.username, $scope.password)
-                       $location.path('/'); 
-                    }else{
-                        $scope.error = "Login Failed"
-                    }
-                    
-                },
-                function error(errorResponse) {
-                    console.log("Error:" + JSON.stringify(errorResponse));
-                }
-        );
+angularBlogControllers.controller('LoginCtrl', ['$scope', '$location', 'Login', 'setCreds',
+    function LoginCtrl($scope, $location, Login, setCreds) {
+        $scope.submit = function () {
+            $scope.sub = true;
+            var postData = {
+                "username": $scope.username,
+                "password": $scope.password
+            };
             
+            Login.login({}, postData,
+                    function success(response) {
+                        console.log("Success:" + JSON.stringify(response));
+                        if (response.authenticated) {
+                            setCreds($scope.username, $scope.password)
+                            $location.path('/');
+                        } else {
+                            $scope.error = "Login Failed"
+                        }
+
+                    },
+                    function error(errorResponse) {
+                        console.log("Error:" + JSON.stringify(errorResponse));
+                    }
+            );
+
         };
     }]);
 
 angularBlogControllers.controller('LogoutCtrl', ['$location', 'deleteCreds',
-    function LogoutCtrl($location, deleteCreds) { 
+    function LogoutCtrl($location, deleteCreds) {
         deleteCreds();
-        $location.path('/login');         
+        $location.path('/login');
     }]);
 
